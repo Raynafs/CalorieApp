e2d7e0666cc57c1d332b34726c7d8f6339a80e5d
Raynafs <raynafs90@gmail.com>
2024-06-06 15:04:21 +0300

    setup spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -9,3 +11,56 @@ plugins {
     alias(libs.plugins.com.diffplug.spotless) apply false
 }
 true
+
+subprojects {
+    apply {
+        plugin("com.diffplug.spotless")
+        plugin(  "jacoco")
+    }
+
+    configure<SpotlessExtension> {
+        val lintVersion = "0.49.0"
+
+        kotlin {
+            target("**/*.kt")
+            ktlint(lintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ktlint_standard_package-name" to "disabled",
+                        "ij_kotlin_allow_trailing_comma" to "false",
+                        "ij_kotlin_allow_trailing_comma_on_call_site" to "false",
+                    ),
+                )
+            ktfmt().googleStyle()
+            licenseHeaderFile("${project.rootProject.projectDir}/license-header.txt")
+            trimTrailingWhitespace()
+            indentWithTabs(2)
+            indentWithSpaces(4)
+
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint(lintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ij_kotlin_allow_trailing_comma" to "false",
+                        "ij_kotlin_allow_trailing_comma_on_call_site" to "false",
+                    )
+                )
+            ktfmt().googleStyle()
+            indentWithTabs(2)
+            indentWithSpaces(4)
+
+        }
+
+        format("xml") {
+            target("**/*.xml")
+            indentWithSpaces()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+    }
+
+}
